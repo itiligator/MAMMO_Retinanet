239b9f2b79f24af5ea9dcb90ec092e182e2ac985
HWEJIN <hwejin23@gmail.com>
2018-03-08 12:41:06 +0900

    -

---
+++ train.sh
@@ -0,0 +1,22 @@
+
+
+python keras_retinanet/bin/train.py --weight 'INBreast' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/GURO/0_0_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'INBreast' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/GURO/0_1_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'INBreast' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/GURO/0_2_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'INBreast' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/GURO/0_3_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'INBreast' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/GURO/0_4_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'GURO' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/INBreast/0_0_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'GURO' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/INBreast/0_1_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'GURO' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/INBreast/0_2_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'GURO' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/INBreast/0_3_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
+python keras_retinanet/bin/train.py --weight 'GURO' csv '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Annotation/FINAL/INBreast/0_4_train.csv' '/media/hwejin/SSD_1/DATA/DATAForPaper_v2/Common/class.csv' '-'
+
